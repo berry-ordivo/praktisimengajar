4a6eda2e8a28f487fb79e0f4c5f5fb692ad724f8
Berry Firmann <clickdisini@gmail.com>
2023-06-01 10:32:00 +0700

    first commit

---
+++ resources/views/register2.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.main')
+
+@section('body')
+    <div class="row justify-content-center">
+        <div class="col-sm-6">
+            <div class="card mt-4">
+                <div class="card-header">
+                  Register
+                </div>
+                <div class="card-body">
+                    <form method="POST" action="{{ route('register') }}">
+                        @csrf
+
+                        <div class="row mb-3">
+                            <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="" required autocomplete="name" autofocus>
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="email" class="col-md-4 col-form-label text-md-end">Email</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="" required autocomplete="email">
+                            </div>
+                        </div>
+
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" required autocomplete="current-password">
+                            </div>
+                        </div>
+
+
+                        <div class="row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Register
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+              </div>
+        </div>
+    </div>
+@endsection
